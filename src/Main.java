7b3ad19362ba59c1046a0a6cce8b81da777b9dd4
Kanykei500 <askarbekovakanykei120@gmail.com>
2022-12-02 15:34:16 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+       String[] alfavit = {"Aa", "Bb", "Cc", "Dd", "Ee", "Ff", "Gg", "Hh", "Ii", "Jj",
+               "Kk", "Ll", "Mm", "Nn", "Oo", "Pp", "Qq", "Rr", "Ss", "Tt", "Uu", "Vv",
+               "Ww", "Xx", "Yy", "Zz", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+        try (FileWriter fileWriter = new FileWriter("alfavit.txt")){
+            for (String a:alfavit) {
+                fileWriter.write(a+"\n ");
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+           int counter = 0;
+           FileReader fileReader=new FileReader("alfavit.txt");
+           Scanner scanner=new Scanner(fileReader);
+           while (scanner.hasNextLine()){
+               counter++;
+               System.out.println(counter+"-"+scanner.nextLine());
+           }
+       }catch (IOException e){
+           e.printStackTrace();
+       }
+   }
 }
